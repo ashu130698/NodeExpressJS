58118b6572fe76e414f0d78640a5f917a9323903
ashu130698 <ashushukla299@gmail.com>
2025-08-18 21:21:56 +0530

    trying to create a server

---
+++ server.js
@@ -0,0 +1,23 @@
+const { log } = require('console');
+let http =require('http');
+
+let server = http.createServer ((req,res)=>{
+if (req.url == "/users") {
+    let email= req.headers.email;
+    res.write(`You are getting a response in user ${email}`);
+    res.end();
+} else if (req.url == "/products") {
+    res.write('You are getting a response in products');
+    res.end();
+} else {
+    res.statusCode=404;
+    res.write('Invalid user');
+    res.end();
+}
+});
+
+let port = 8080;
+
+server.listen(port,()=>{
+    console.log(`server is listening at ${port}`)
+});
