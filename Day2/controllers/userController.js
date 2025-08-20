b1d1a47d1cfb9af1d78543fb4454f359b84b2031
ashu130698 <ashushukla299@gmail.com>
2025-08-20 22:13:46 +0530

    add updateUser and deletuser

--- Day2/controllers/userController.js
+++ Day2/controllers/userController.js
@@ -3,7 +3,7 @@ const {
     SUCCESS,
     CREATED,
     NOT_FOUND
-} = REQUIRE("../utils/constants");
+} = require("../utils/constant");
 
 let userDB = [];
 
@@ -27,7 +27,7 @@ exports.getUserById = (req, res)=> {
             return;
         }
     }
-    res.status(NOT_FOUND).JSON({
+    res.status(NOT_FOUND).json({
         status: "failure",
         message: `User with id ${id} not found`,
     });
@@ -60,9 +60,63 @@ exports.createUser = (req, res)=> {
         data:user
     });
 };
+// update user by username
+exports.updateUser = (req, res) => {
+    const {username,email,phone,password}=req.body;
 
-exports.updateUser = (req, res) = {};
+    if (!username) {
+        res.status(BAD_REQUEST).json({
+            status:"failure",
+            message :"Username is require to update user",
+        });
+        return;
+    }
+     for(let i=0; i< userDB.length; i++) {
+        if (userDB[i].username === username) {
+            if (email) userDB[i].email=email;
+            if (password) userDB.password=password;
+            if (phone) userDB.phone=phone;
 
-exports.deleteAllUsers = (req, res) = {};
+            res.status(SUCCESS).json({
+                status:"success",
+                message:`User ${username} updated successfully`,
+                date: userDB[i],
+            });
+            return;
+        }
+     }
+     res.status(NOT_FOUND).json({
+        status:"success",
+        message:"User with username ${username} not found",
+     });
+};
+// delete all user
+exports.deleteAllUsers = (req, res) => {
+    userDB=[];
+    res.status(SUCCESS).json({
+        status:"success",
+        message:"All users delete successfully",
+    });
+    
+};
+// delete user by id
+exports.deleteUserById = (req, res) => {
+    const id = req.params.id;
 
-exports.deleteUserById = (req, res) = {};
+    for(let i=0; i< userDB.length; i++) {
+        if (userDB[i].username===id) {
+            const deleteUser = userDB.splice(i,1);
+            res.status(SUCCESS).json({
+                status:"success",
+                message: `User ${id} dlete successfully`,
+                data:delete[0],
+            });
+            return;
+        }
+    }
+
+    res.status(NOT_FOUND).json({
+        status:"failure",
+        message:"User with username ${id} not found",
+    });
+};
