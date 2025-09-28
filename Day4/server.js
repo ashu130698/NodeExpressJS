02a91cc947d852785a95ad2e61e3c3e0fa214ee3
ashu130698 <ashushukla299@gmail.com>
2025-09-28 23:21:22 +0530

    working with server

---
+++ Day4/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+const mongoose = require("mongoose");
+
+const app = express();
+const port = 8080;
+const server = app.listen(port, ()=> {
+    console.log(`Server is running at port ${port}`);
+});
+
+const DB_URL=process.env.DB_URL.replace(
+    "<password>",process.env.DB_PASSWORD
+);
+
+const DB = mongoose.connect(DB_URL) .then(()=>
+    {console.log("Connected to Database! 💕");
+});
