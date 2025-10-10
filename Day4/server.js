e7be71ae292d84849ae27d572c8011566c12d956
ashu130698 <ashushukla299@gmail.com>
2025-10-10 23:08:23 +0530

    make code reusable

--- Day4/server.js
+++ Day4/server.js
@@ -1,18 +1,16 @@
-const express = require("express");
-const dotenv = require("dotenv");
-dotenv.config({ path: "./.env" });
 const mongoose = require("mongoose");
+const app = require("./app");
 
-const app = express();
 const port = 8080;
-const server = app.listen(port, ()=> {
-    console.log(`Server is running at port ${port}`);
+const server = app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 
-const DB_URL=process.env.DB_URL.replace(
-    "<password>",process.env.DB_PASSWORD
+const DB_URL = process.env.DB_URL.replace(
+  "<password>",
+  process.env.DB_PASSWORD
 );
 
-const DB = mongoose.connect(DB_URL) .then(()=>
-    {console.log("Connected to Database! 💕");
+const DB = mongoose.connect(DB_URL).then(() => {
+  console.log("Connected to DB❤");
 });
