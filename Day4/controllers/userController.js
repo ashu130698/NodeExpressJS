ffa5b0d66fd96169816de1d37a69a59f4094db3d
ashu130698 <ashushukla299@gmail.com>
2025-10-15 23:14:40 +0530

    getAll users

--- Day4/controllers/userController.js
+++ Day4/controllers/userController.js
@@ -1,19 +1,35 @@
 const UserModel = require("../models/userModel");
 
-exports.getAllUsers = async function (req, res) {};
+exports.getAllUsers = async function (req, res) {
+  let users;
+  try {
+    users = await UserModel.find();
+  } catch (err) {
+    res.status(500).json({
+      status: "fail",
+      message: err.message,
+    });
+    return;
+  }
+  res.status(200).json({
+    status: "success",
+    data: users,
+  });
+};
 
 exports.getUserById = async function (req, res) {};
 
 exports.createUser = async function (req, res) {
   const { username, email, password, role } = req.body;
-    let user;
+  let user;
   try {
     user = await UserModel.create({ username, email, password, role, });
   } catch (error) {
       res.status(500).json({
           status: "fail",
           message: error.message,
-      })
+      });
+    return;
     }
     
     res.status(201).json({
