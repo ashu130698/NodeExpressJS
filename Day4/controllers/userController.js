2aaeedeb7bf6f78e160e8d64a3a76adc04b76d93
ashu130698 <ashushukla299@gmail.com>
2025-10-18 23:31:18 +0530

    create user by id

--- Day4/controllers/userController.js
+++ Day4/controllers/userController.js
@@ -3,7 +3,7 @@ const UserModel = require("../models/userModel");
 exports.getAllUsers = async function (req, res) {
   let users;
   try {
-    users = await UserModel.find();
+    users = await UserModel.find().select("-__v");
   } catch (err) {
     res.status(500).json({
       status: "fail",
@@ -13,11 +13,27 @@ exports.getAllUsers = async function (req, res) {
   }
   res.status(200).json({
     status: "success",
+    const : users.length,
     data: users,
   });
 };
 
-exports.getUserById = async function (req, res) {};
+exports.getUserById = async function (req, res) {
+  let user;
+  try {
+    user = await UserModel.findById(id);
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: err.message,
+    });
+    return;
+  }
+  res.status(200).json({
+    status: "success",
+    data: user,
+  });
+};
 
 exports.createUser = async function (req, res) {
   const { username, email, password, role } = req.body;
